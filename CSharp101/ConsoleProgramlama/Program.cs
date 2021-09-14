6efa3032669d17a99691cd02ee5ce2aa4d9b8e5d
Gizem Cimen <gizemcimen.cg@gmail.com>
2021-09-15 00:50:04 +0300

    gitignore dosyası ve CSharp101 dosyaları yüklendi

---
+++ CSharp101/ConsoleProgramlama/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleProgramlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("İsminiz: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminiz: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
+            Console.ReadLine();
+        }
+    }
+}
