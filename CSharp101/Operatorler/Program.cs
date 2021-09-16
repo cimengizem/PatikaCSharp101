de2194bcb38f124c7dae195192b93bf81aaa7bfa
Gizem Cimen <gizemcimen.cg@gmail.com>
2021-09-16 23:28:12 +0300

    Operatorler dersi tamamlandi.

---
+++ CSharp101/Operatorler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
